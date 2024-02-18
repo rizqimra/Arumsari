afdeaf7f532f300aa9deeeea6fc4383f729c3dae
rizqimra <rizqimrazhari@gmail.com>
2024-02-18 17:41:46 +0800

    features: styling hero section

--- script.js
+++ script.js
@@ -2,11 +2,22 @@ const navLinks = document.querySelector(".nav-links");
 const hamburger = document.querySelector(".hamburger");
 
 hamburger.addEventListener("click", () => {
-  navLinks.classList.toggle('active')
+  navLinks.classList.toggle("active");
+});
+
+navLinks.addEventListener("click", () => {
+  navLinks.classList.remove("active");
 });
 
 document.addEventListener("click", (e) => {
   if (!navLinks.contains(e.target) && !hamburger.contains(e.target)) {
     navLinks.classList.remove("active");
   }
+});
+
+window.addEventListener("scroll", () => {
+  let value = window.scrollY;
+  let text = document.querySelector(".hero .content");
+
+  text.style.marginTop = value * 2.4 + "px";
 });
