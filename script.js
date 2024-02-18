bf5081f6ed9fb227907f22301f8e1041497e1406
rizqimra <rizqimrazhari@gmail.com>
2024-02-18 14:15:50 +0800

    fix: project structure and navbar styling

--- script.js
+++ script.js
@@ -1,29 +1,12 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const menuToggle = document.querySelector(".menu-toggle");
-  const navLink = document.querySelectorAll(".nav-link");
-  const navLinks = document.querySelector(".nav-links");
+const navLinks = document.querySelector(".nav-links");
+const hamburger = document.querySelector(".hamburger");
 
-  function removeActiveClass() {
-    navLink.forEach((link) => {
-      link.classList.remove("active");
-    });
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle('active')
+});
+
+document.addEventListener("click", (e) => {
+  if (!navLinks.contains(e.target) && !hamburger.contains(e.target)) {
+    navLinks.classList.remove("active");
   }
-
-  navLink.forEach((link) => {
-    link.addEventListener("click", () => {
-      removeActiveClass();
-      link.classList.add("active");
-      document.body.classList.remove("nav-open");
-    });
-  });
-
-  menuToggle.addEventListener("click", () => {
-    navLinks.classList.toggle("nav-open");
-  });
-
-  document.addEventListener("click", (event) => {
-    if (!navLinks.contains(event.target) && !menuToggle.contains(event.target)) {
-      navLinks.classList.remove("nav-open");
-    }
-  });
 });
